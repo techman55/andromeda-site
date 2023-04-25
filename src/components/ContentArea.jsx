fe6d9255ae73e604fc4848a84d1b71cec5cda73a
Jack Hubbard <jack@techman.dev>
2023-04-25 13:29:57 -0500

    /: Content Area Size Adjustment for mobile
    Signed-off-by: Jack Hubbard <jack@techman.dev>

--- src/components/ContentArea.jsx
+++ src/components/ContentArea.jsx
@@ -3,7 +3,7 @@ export default function ContentArea(props) {
     const { body, image, title, header } = props
 
 
-    return <div className={"flex flex-row justify-evenly border-2 border-[#5243ac] rounded-xl bg-gradient-to-r from-[#333333] to-[#5243ac] p-6 m-10 mt-20 flex-wrap drop-shadow-xl"}>
+    return <div className={"flex flex-row justify-evenly border-2 border-[#5243ac] rounded-xl bg-gradient-to-r from-[#333333] to-[#5243ac] p-6 m-5 mt-20 flex-wrap drop-shadow-xl"}>
 
         <div className={"flex sm:w-1/2 flex-col  justify-start"}>
 
